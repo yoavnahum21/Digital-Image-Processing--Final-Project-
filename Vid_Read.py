e5685a21a9cca5ceedaff0e393d9e85b5b0c0c37
yoavnahum21 <98908009+yoavnahum21@users.noreply.github.com>
2024-03-02 12:37:12 +0200

    Reading frames from camera successfully

--- Vid_Read.py
+++ Vid_Read.py
@@ -2,8 +2,18 @@ import numpy as np
 import cv2
 
 
-def Read_video_from_live_cam() -> np.array: # יש מצב מחזיר משתנה אחר של cv2
-    pass
+class Video_Read:
+    def __init__(self, device_num):
+        cv2.namedWindow("Digital image processing")
+        self.vc = cv2.VideoCapture(device_num)
+
+    def Read_video_from_live_cam(self) -> np.array: # יש מצב מחזיר משתנה אחר של cv2
+
+        rval, frame = self.vc.read()
+        return frame
+
+        # self.vc.release()
+        # cv2.destroyWindow("Digital Image Processing")
 
 
 def Current_Processed_frame() -> np.array: # יש מצב מחזיר משתנה אחר של cv2
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-import Vid_Read
+from Vid_Read import Video_Read
 import comm_platform
+import cv2
 
 
 def main():
+    camera_hands = Video_Read(0)
     while True:
-        pass
+        hands_frame = camera_hands.Read_video_from_live_cam()
+        cv2.imshow("Digital Image Processing", hands_frame)
+        # cv2.waitKey(20)
     
 
 if __name__ == '__main__':
