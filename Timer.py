ddf2aa709128f9fa7513ec74ff30d18dd4bd4dd2
Nikolai Krokhmal <kroniko64@gmail.com>
2024-04-03 17:30:13 +0300

    debugged timer a bit
    Signed-off-by: Nikolai Krokhmal <kroniko64@gmail.com>

--- Timer.py
+++ Timer.py
@@ -13,23 +13,23 @@ class TimerError(Exception):
 
 class Timer:
     def __init__(self):
-        self._start_time = None
+        self.start_time = None
 
     def start(self):
         """Start a new timer"""
-        if self._start_time is not None:
+        if self.start_time is not None:
             raise TimerError(f"Timer is running. Use .stop() to stop it")
 
-        self._start_time = time.perf_counter()
+        self.start_time = time.perf_counter()
 
     def stop(self):
         """Stop the timer, and report the elapsed time"""
-        if self._start_time is None:
+        if self.start_time is None:
             raise TimerError(f"Timer is not running. Use .start() to start it")
-        self._start_time = None
+        self.start_time = None
 
     def get_timer(self):
-        return round(time.perf_counter() - self._start_time, 3)
+        return round(time.perf_counter() - self.start_time, 3)
 
 
 if __name__ == '__main__':
