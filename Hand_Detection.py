11b7ef2a638bfd5c7f1a8543c74072e918ce5210
Nikolai Krokhmal <kroniko64@gmail.com>
2024-04-04 18:20:30 +0300

    some work on the hand detection file - the player class and a placeholder for hand detection method
    Signed-off-by: Nikolai Krokhmal <kroniko64@gmail.com>

--- Hand_Detection.py
+++ Hand_Detection.py
@@ -1,14 +1,22 @@
 import cv2
 
+''' This file contains the player class with the following attributes and methods:
+1. name - self explanatory
+2. get_curr_pos - reads camera frame and determines hand position.'''
+
 
 class Player:
-    def __init__(self, name, zero_pos, max_pos, min_pos):
+    def __init__(self, name):
         self.name = name
-        self.zero_pos = zero_pos
-        self.max_pos = max_pos
-        self.min_pos = min_pos
+        self.zero_pos = None
+        self.max_pos = None
+        self.min_pos = None
 
+    def get_curr_pos(self):
+        # TODO: write method to read frame from camera and detect hand position
+        pass
 
+    
 if __name__ == '__main__':
     def image_show(frame):
         edges = cv2.Canny(frame, 100, 200)
