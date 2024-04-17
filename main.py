546905db42cebba1748f01fc08e64439352fd59b
Dor David <dordavid2412@gmail.com>
2024-04-17 16:28:26 +0300

    17/4 . 16:30

--- main.py
+++ main.py
@@ -13,14 +13,11 @@ def calibration():
         frame = camera_hands.Read_video_from_live_cam()
         rect1_frame = cv2.rectangle(frame, (60, 140), (240, 340), (0, 255, 0), 4)
         rect2_frame = cv2.rectangle(rect1_frame, (400, 140), (580, 340), (0, 255, 0), 4)
-        # rect1_frame = cv2.circle(rect2_frame,(490,240), 80,(255,0,0),2)
 
         flipped_image = cv2.flip(rect1_frame, 1)
 
         left_hand = flipped_image[140:340, 60:240, :]
         right_hand = flipped_image[140:340, 400:580, :]
-        left_hand_gauss = cv2.GaussianBlur(left_hand, ksize=(3, 3), sigmaX=0)
-        right_hand_gauss = cv2.GaussianBlur(right_hand, ksize=(3, 3), sigmaX=0)
         cv2.imshow("rec_frame", flipped_image)
 
         count += 1
@@ -28,12 +25,113 @@ def calibration():
         if count == 100:
             return left_hand, right_hand
 
+def sift(hands_frame, result):
+    frame_check_RT_gray = cv2.cvtColor(hands_frame, cv2.COLOR_BGR2GRAY)  ## to bitwise multiplication
+
+    frame_check_RT = cv2.cvtColor(hands_frame, cv2.COLOR_BGR2HSV)
+    frame_check_RT_h = frame_check_RT[:, :, 0]
+    frame_check_RT_s = frame_check_RT[:, :, 1]
+
+    _, frame_check_RT_s_masked = cv2.threshold(frame_check_RT_s, 40, 255,
+                                               cv2.THRESH_BINARY)  ## to bitwise multiplication
+
+    result_RT = cv2.bitwise_and(frame_check_RT_gray, frame_check_RT_s_masked)
+
+    hands_frame_RT = cv2.flip(result_RT, 1)
+
+    sift = cv2.SIFT_create(nOctaveLayers=3, contrastThreshold=0.04, edgeThreshold=10, sigma=1.6)
+    sift.setEdgeThreshold(1000)
+
+    keypoints_reference, descriptors_reference = sift.detectAndCompute(result, None)  ## reference hands_frame
+
+    keypoints_RT, descriptors_RT = sift.detectAndCompute(hands_frame_RT, None)  ## RT hands_frame
+
+    matcher = cv2.BFMatcher()  # match between the photos
+    if descriptors_RT is not None:
+        matches = matcher.knnMatch(descriptors_reference, descriptors_RT, k=2)
+        good_matches = []
+        good_keypoints_x = []
+        good_keypoints_y = []
+        for m, n in matches:
+            if m.distance < 0.7 * n.distance:
+                good_matches.append(m)
+                good_keypoints_x.append(round(keypoints_RT[m.trainIdx].pt[0]))
+                good_keypoints_y.append(round(keypoints_RT[m.trainIdx].pt[1]))
+        hands_frame_RT = cv2.drawMatches(result, keypoints_reference, hands_frame_RT, keypoints_RT,
+                                             good_matches, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    else:
+        cv2.imshow('Matched Keypoints', hands_frame_RT)
+        cv2.waitKey(20)
+        return None
+
+    cv2.imshow('Matched Keypoints', hands_frame_RT)
+    cv2.waitKey(20)
+
+    good_keypoints = list(zip(good_keypoints_y, good_keypoints_x))
+    if len(good_matches) < 6:
+        cv2.imshow('Matched Keypoints', hands_frame_RT)
+        cv2.waitKey(20)
+        return None
+    left_good_keypoints = [left for left in good_keypoints if left[1] < 280]
+    right_good_keypoints = [right for right in good_keypoints if right[1] >= 360]
+
+    first_elements_left_x = [item[0] for item in left_good_keypoints]
+    first_elements_left_y = [item[1] for item in left_good_keypoints]
+
+    first_elements_right_x = [item[0] for item in right_good_keypoints]
+    first_elements_right_y = [item[1] for item in right_good_keypoints]
+
+    if len(left_good_keypoints) == 0 or len(right_good_keypoints) == 0:
+        cv2.imshow('Matched Keypoints', hands_frame_RT)
+        cv2.waitKey(20)
+        return None
+
+    else:
+        centroid_left_x = sum(first_elements_left_x) // len(left_good_keypoints)
+        centroid_left_y = sum(first_elements_left_y) // len(left_good_keypoints)
+
+        centroid_right_x = sum(first_elements_right_x) // len(right_good_keypoints)
+        centroid_right_y = sum(first_elements_right_y) // len(right_good_keypoints)
+
+    centroid_left = (centroid_left_y + 360, centroid_left_x)
+    centroid_right = (centroid_right_y + 360, centroid_right_x)
+
+    cv2.line(hands_frame_RT, centroid_left, centroid_right, (0, 255, 0), 3)
+    cv2.imshow('Matched Keypoints', hands_frame_RT)
+    cv2.waitKey(20)
+
+    slope = -(centroid_right[1] - centroid_left[1]) / (centroid_right[0] - centroid_left[0])
+
+    print("Slope of the line:", slope)
+    return  slope
+
+
+
+def fsm(grad, port):
+
+    if grad is None:
+        comm_platform.Set_package_and_transmit('r', port) # stop car
+    elif (grad <= 0.2) and (grad >= -0.2):
+        comm_platform.Set_package_and_transmit('w', port) # go forward
+    elif (grad < -0.2) and (grad >= -0.5):
+        comm_platform.Set_package_and_transmit('y', port) # go forward and right
+    elif (grad > 0.2) and (grad <= 0.5):
+        comm_platform.Set_package_and_transmit('t', port) # go forward and left
+    elif grad > 0.5:
+        comm_platform.Set_package_and_transmit('a', port)  # go right
+    elif grad < -0.5:
+        comm_platform.Set_package_and_transmit('d', port)  # go right
+    else:
+        return
+    cv2.waitKey(50)
+    comm_platform.Set_package_and_transmit('r', port)
+    return
+
 
 # main func
 def main():
-    # port = comm_platform.init_port()
-    # comm_platform.Set_package_and_transmit('r', port)
-    hands_frame = camera_hands.Read_video_from_live_cam()
+    port = comm_platform.init_port()
+    comm_platform.Set_package_and_transmit('r', port)
     left_hand_ref, right_hand_ref = calibration()
 
     concatenated_image = cv2.hconcat([left_hand_ref, right_hand_ref])
@@ -42,50 +140,14 @@ def main():
     cv2.waitKey(0)
 
     frame_check = cv2.cvtColor(concatenated_image, cv2.COLOR_BGR2HSV)
-    cv2.imshow('HSV Image ', frame_check)
-    cv2.waitKey(0)
 
     frame_check_s = frame_check[:, :, 1]
     cv2.imshow('HSV Image ssss', frame_check_s)
     cv2.waitKey(0)
 
-    frame_check_h = 255 - frame_check[:, :, 2]
-    cv2.imshow('HSV Image vvvv', frame_check_h)
-    cv2.waitKey(0)
-
-    # plt.figure(figsize=(8, 12))
-    # histogram_h = cv2.calcHist([frame_check_h], [0], None, [256], [0, 256])
-    # histogram_s = cv2.calcHist([frame_check_s], [0], None, [256], [0, 256])
-    #
-    #
-    # # Plot S channel histogram
-    # plt.subplot(2, 1, 1)
-    # plt.plot(histogram_s, color='blue')
-    # plt.title('Histogram of S (Saturation) Channel')
-    # plt.xlabel('S Value')
-    # plt.ylabel('Frequency')
-    # plt.grid(True)
-    #
-    # # Plot H channel histogram
-    # plt.subplot(2, 1, 2)
-    # plt.plot(histogram_h, color='green')  # Using green color for H channel
-    # plt.title('Histogram of H (Hue) Channel')
-    # plt.xlabel('H Value')
-    # plt.ylabel('Frequency')
-    # plt.grid(True)
-    #
-    # plt.tight_layout()  # Adjust layout for better spacing
-    # plt.show() ## histograms
-
-    # _, frame_check_s = cv2.threshold(frame_check_s, 40, 130, cv2.THRESH_BINARY)
     _, frame_check_s_masked = cv2.threshold(frame_check_s, 40, 255, cv2.THRESH_BINARY)  ## to bitwise multiplication
 
     cv2.imshow('channel s mask', frame_check_s_masked)
-    cv2.waitKey(0)
-
-    # _, frame_check_h = cv2.threshold(frame_check_h, 20, 60, cv2.THRESH_BINARY)
-    _, frame_check_h_masked = cv2.threshold(frame_check_h, 50, 255, cv2.THRESH_BINARY)
-    cv2.imshow('channel h mask', frame_check_h_masked)
     cv2.waitKey(0)
 
     # Concatenate the images vertically
@@ -101,73 +163,38 @@ def main():
     cv2.imshow('result', result)
     cv2.waitKey(0)
 
+    # sift = cv2.SIFT_create(nfeatures=100, nOctaveLayers=3, contrastThreshold=0.04, edgeThreshold=10, sigma=1.6)
+    # sift.setEdgeThreshold(1000)
+    #
+    # keypoints_reference, descriptors_reference = sift.detectAndCompute(result, None)  ## reference hands_frame
+    #
+    # imgKp1 = cv2.drawKeypoints(result, keypoints_reference,None)
+    # cv2.imshow('imgkp1', imgKp1)
+    # cv2.waitKey(0)
+    #
+    # keypoint_locations = []
+    #
+    # for keypoint in keypoints_reference:
+    #     x, y = keypoint.pt  # Get the (x, y) coordinates of the keypoint
+    #     x = round(x)
+    #     y = round(y)
+    #     keypoint_locations.append((x, y))  # Append the coordinates to the list
+    #
+    # print("Keypoint Locations:", keypoint_locations)
+    #
+    #
+
+
     while True:
 
         hands_frame = camera_hands.Read_video_from_live_cam()
-        hands_frame = cv2.rectangle(hands_frame, (200, 0), (480, 480), (0, 255, 0), 4)
+        grad = sift(hands_frame, result)
+        fsm(grad,port)
 
-        frame_check_RT_gray = cv2.cvtColor(hands_frame, cv2.COLOR_BGR2GRAY)  ## to bitwise multiplication
 
-        frame_check_RT = cv2.cvtColor(hands_frame, cv2.COLOR_BGR2HSV)
-        frame_check_RT_h = frame_check_RT[:, :, 0]
-        frame_check_RT_s = frame_check_RT[:, :, 1]
 
-        _, frame_check_RT_s = cv2.threshold(frame_check_RT_s, 40, 130, cv2.THRESH_BINARY)
-        _, frame_check_RT_s_masked = cv2.threshold(frame_check_RT_s, 40, 255,
-                                                   cv2.THRESH_BINARY)  ## to bitwise multiplication
 
-        _, frame_check_RT_h_masked = cv2.threshold(frame_check_RT_h, 30, 255, cv2.THRESH_BINARY_INV)
 
-        result_RT = cv2.bitwise_and(frame_check_RT_gray, frame_check_RT_s_masked)
-
-        hands_frame_RT = cv2.flip(result_RT, 1)
-        #
-        # x1, y1 = 200, 200  # Top-left corner
-        # x2, y2 = 400, 400  # Bottom-right corner
-        #
-        # black_rect = np.zeros_like(hands_frame_RT)
-        #
-        # # Draw a white rectangle on the black image to create the mask
-        # cv2.rectangle(black_rect, (x1, y1), (x2, y2), (255, 255, 255), -1)
-        #
-        # # Use the bitwise NOT operation to invert the mask (black becomes white, white becomes black)
-        # mask = cv2.bitwise_not(black_rect)
-        #
-        # # Use the bitwise AND operation to combine the mask with the original image
-        # hands_frame_RT = cv2.bitwise_and(hands_frame_RT, mask)
-
-        sift = cv2.SIFT_create()
-        sift.setEdgeThreshold(1000)
-
-        keypoints_reference, descriptors_reference = sift.detectAndCompute(result, None)  ## reference hands_frame
-
-        # mask = np.ones_like(hands_frame_RT, dtype=np.uint8) * 255  # Initialize with all white
-        # mask[0:500, 200:500] = 0
-
-        keypoints_RT, descriptors_RT = sift.detectAndCompute(hands_frame_RT, None)  ## RT hands_frame
-
-        matcher = cv2.BFMatcher()  # match between the photos
-        if descriptors_RT is not None:
-            matches = matcher.knnMatch(descriptors_reference, descriptors_RT, k=2)
-            good_matches = []
-            for m, n in matches:
-                if m.distance < 0.75 * n.distance:
-                    good_matches.append(m)
-            matched_image_left = cv2.drawMatches(result, keypoints_reference, hands_frame_RT, keypoints_RT,
-                                                 good_matches, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-
-        cv2.imshow('Matched Keypoints', matched_image_left)
-        # cv2.imshow('imgKp1',imgKp1)
-        # cv2.imshow('imgKp2', imgKp2)
-        cv2.waitKey(20)
-        #
-        print(len(good_matches))
-
-    #
-    #
-    #     # print (descriptors_reference.shape, descriptors_RT.shape)
-    #     # print( len(good_matches))
-    #     # # ##########################
     #     # print("Enter your desired direction:")
     #     # signal = input()
     #     # # hands_frame = camera_hands.Read_video_from_live_cam()
